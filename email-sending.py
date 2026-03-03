ed3182d013378daffac8d88c8b55579f33bf5668
Joanna <joannasun1996@gmail.com>
2026-03-03 11:10:16 -0500

    Update email-sending.py

--- email-sending.py
+++ email-sending.py
@@ -1,22 +1,20 @@
-# smtplib is a library, Simple Mail Transfer Protocol. It allows you to send emails using Gmail or other mail services.
 import smtplib
-# Without from email.message import EmailMessage, you cannot do msg = EmailMessage() msg["Subject"] = "Hi!" msg["From"] = your_email msg["To"] = Person_your_sending_email to's_email msg.set_content("Have a great day!")
 from email.message import EmailMessage
 
 # -------- YOUR DETAILS --------
-your_email = "your_gmail"
-app_password = "your_app_password"
-Person_your_sending_email to's_email = "Person's_gmail"
+your_email = "yourgmail@gmail.com"
+app_password = "your_app_password_here"
+someone_email = "someone@gmail.com"
 # ------------------------------
 
-# Create the email. msg is short for message.
+# Create the email
 msg = EmailMessage()
-msg["Subject"] = "Hi!"
+msg["Subject"] = "Hey!"
 msg["From"] = your_email
-msg["To"] = Person_your_sending_email to's_email
+msg["To"] = someone_email
 msg.set_content("Have a great day!")
 
-# Connect to Gmail SMTP server and send. Server is a computer on the internet, that does services.
+# Connect to Gmail SMTP server and send
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(your_email, app_password)
     server.send_message(msg)
